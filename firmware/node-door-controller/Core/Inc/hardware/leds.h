d96abc1b5b0155a955bb8d19f9926e479e09f137
Robert Paetzholdt <paetzholdt@posteo.de>
2026-05-13 01:42:25 +0200

    refactor: replace duplicated LED GPIO handling with generic set_led implementation and Led_t struct

--- firmware/node-door-controller/Core/Inc/hardware/leds.h
+++ firmware/node-door-controller/Core/Inc/hardware/leds.h
@@ -1,8 +1,20 @@
 #ifndef LEDS_H
 #define LEDS_H
 
+#include "stm32g0xx_hal.h"
+
 
 #include <stdbool.h>
+
+
+typedef struct {
+	GPIO_TypeDef *port;
+	uint16_t pin;
+
+	bool current_state;
+
+} Led_t;
+
 
 void set_door_release_led(bool shall_be_on);
 
--- firmware/node-door-controller/Core/Src/hardware/leds.c
+++ firmware/node-door-controller/Core/Src/hardware/leds.c
@@ -4,60 +4,52 @@
 
 #include <stdbool.h>
 
-// TODO: write generic set_led(*led)
 
-void set_door_release_led(bool shall_be_on) {
+Led_t door_release_led = {
+		.port = DOOR_RELEASE_LED_GPIO_Port,
+		.pin = DOOR_RELEASE_LED_Pin,
+		.current_state = false
+};
 
-	static bool current_led_state = false;
+Led_t stop_request_led = {
+		.port = STOP_REQUEST_LED_GPIO_Port,
+		.pin = STOP_REQUEST_LED_Pin,
+		.current_state = false
+};
 
-	if (current_led_state == shall_be_on) {
+Led_t green_loop_led = {
+		.port = GREEN_LOOP_LED_GPIO_Port,
+		.pin = GREEN_LOOP_LED_Pin,
+		.current_state = false
+};
+
+
+void set_led(Led_t* led, bool shall_be_on) {
+	if (led->current_state == shall_be_on) {
 		return;
 	}
 
 	if (shall_be_on) {
-		HAL_GPIO_WritePin(DOOR_RELEASE_LED_GPIO_Port, DOOR_RELEASE_LED_Pin, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(led->port, led->pin, GPIO_PIN_SET);
 	} else {
-		HAL_GPIO_WritePin(DOOR_RELEASE_LED_GPIO_Port, DOOR_RELEASE_LED_Pin, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(led->port, led->pin, GPIO_PIN_RESET);
 	}
 
-	current_led_state = shall_be_on;
+	led->current_state = shall_be_on;
+}
+
+void set_door_release_led(bool shall_be_on) {
+	set_led(&door_release_led, shall_be_on);
 }
 
 
 void set_stop_request_led(bool shall_be_on) {
-
-	static bool current_led_state = false;
-
-	if (current_led_state == shall_be_on) {
-		return;
-	}
-
-	if (shall_be_on) {
-		HAL_GPIO_WritePin(STOP_REQUEST_LED_GPIO_Port, STOP_REQUEST_LED_Pin, GPIO_PIN_SET);
-	} else {
-		HAL_GPIO_WritePin(STOP_REQUEST_LED_GPIO_Port, STOP_REQUEST_LED_Pin, GPIO_PIN_RESET);
-	}
-
-	current_led_state = shall_be_on;
+	set_led(&stop_request_led, shall_be_on);
 }
 
 
 void set_green_loop_led(bool shall_be_on) {
-
-	static bool current_led_state = false;
-
-	if (current_led_state == shall_be_on) {
-		return;
-	}
-
-
-
-	if (shall_be_on) {
-		HAL_GPIO_WritePin(GREEN_LOOP_LED_GPIO_Port, GREEN_LOOP_LED_Pin, GPIO_PIN_SET);
-	} else {
-		HAL_GPIO_WritePin(GREEN_LOOP_LED_GPIO_Port, GREEN_LOOP_LED_Pin, GPIO_PIN_RESET);
-	}
-
-	current_led_state = shall_be_on;
+	set_led(&green_loop_led, shall_be_on);
 }
+
 
