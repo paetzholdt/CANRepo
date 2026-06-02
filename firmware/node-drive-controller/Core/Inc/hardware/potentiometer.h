3c447be4b4f05478678cdcaa8e206ef93ec124a3
Robert Paetzholdt <paetzholdt@posteo.de>
2026-06-02 23:59:18 +0200

    feat(node-drive-controller): add potentiometer init and status-based getter API

--- firmware/node-drive-controller/Core/Inc/hardware/potentiometer.h
+++ firmware/node-drive-controller/Core/Inc/hardware/potentiometer.h
@@ -1,6 +1,17 @@
 #ifndef POTENTIOMETER_H
 #define POTENTIOMETER_H
 
+#include <stdint.h>
+
+typedef enum {
+	POTENTIOMETER_STATUS_OK,
+	POTENTIOMETER_STATUS_ERROR
+} PotentiometerStatus_t;
+
+void potentiometer_init(void);
+
+PotentiometerStatus_t get_adc_value(uint32_t *value);
+
 void potentiometer_task(void);
 
 #endif
--- firmware/node-drive-controller/Core/Src/hardware/potentiometer.c
+++ firmware/node-drive-controller/Core/Src/hardware/potentiometer.c
@@ -1,6 +1,7 @@
 #include "hardware/potentiometer.h"
 
 #include <stdio.h>
+#include <stdint.h>
 
 #include "main.h"
 
@@ -9,7 +10,9 @@ extern ADC_HandleTypeDef hadc1;
 static HAL_StatusTypeDef adc_status;
 static uint32_t adc_value;
 
-void potentiometer_task(void) {
+static PotentiometerStatus_t potentiometer_status;
+
+static PotentiometerStatus_t read_potentiometer(void) {
 	// instructions for ADC found in Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_adc.c at line 137 ff.
 
 	// TODO: implement check for errors
@@ -18,6 +21,9 @@ void potentiometer_task(void) {
 	adc_status = HAL_ADC_PollForConversion(&hadc1, 5);
 	if (adc_status != HAL_OK) {
 		printf("Error during adc-poll\r\n");
+		HAL_ADC_Stop(&hadc1);
+		return POTENTIOMETER_STATUS_ERROR;
+
 	} else {
 		adc_value = HAL_ADC_GetValue(&hadc1);
 		printf("ADC-Wert ist: %lu\r\n", adc_value);
@@ -25,4 +31,29 @@ void potentiometer_task(void) {
 
 	HAL_ADC_Stop(&hadc1);
 
+	return POTENTIOMETER_STATUS_OK;
 }
+
+
+void potentiometer_init(void) {
+	potentiometer_status = read_potentiometer();
+	if (potentiometer_status == POTENTIOMETER_STATUS_ERROR) {
+		printf("Error during initialization of potentiometer\r\n");
+	}
+}
+
+PotentiometerStatus_t get_adc_value(uint32_t *value) {
+	if (potentiometer_status == POTENTIOMETER_STATUS_OK) {
+		*value = adc_value;
+	}
+	return potentiometer_status;
+}
+
+void potentiometer_task(void) {
+	potentiometer_status = read_potentiometer();
+
+	if (potentiometer_status == POTENTIOMETER_STATUS_ERROR) {
+		printf("Error during reading of potentiometer\r\n");
+	}
+}
+
