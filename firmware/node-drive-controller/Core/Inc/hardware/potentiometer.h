ff218aa1c8761cff013d22106ebb8209cf4864ba
Robert Paetzholdt <paetzholdt@posteo.de>
2026-06-17 01:07:17 +0200

    feat(node-drive-controller): add potentiometer ADC readout status tracking

--- firmware/node-drive-controller/Core/Inc/hardware/potentiometer.h
+++ firmware/node-drive-controller/Core/Inc/hardware/potentiometer.h
@@ -4,14 +4,18 @@
 #include <stdint.h>
 
 typedef enum {
-	POTENTIOMETER_STATUS_OK,
-	POTENTIOMETER_STATUS_ERROR
+	POTENTIOMETER_STATUS_ERROR,
+	POTENTIOMETER_STATUS_NOT_AVAILABLE,
+	POTENTIOMETER_STATUS_INVALID_ARGUMENT,
+	POTENTIOMETER_STATUS_OK
 } PotentiometerStatus_t;
 
-void potentiometer_init(void);
 
 PotentiometerStatus_t get_potentiometer_value(uint32_t *value);
+PotentiometerStatus_t get_last_attempted_sample_tick(uint32_t *last_attempted_sample_tick_out);
+PotentiometerStatus_t get_last_valid_sample_tick(uint32_t *last_valid_sample_tick_out);
 
-void potentiometer_task(void);
+PotentiometerStatus_t potentiometer_init(void);
+PotentiometerStatus_t potentiometer_task(void);
 
 #endif
--- firmware/node-drive-controller/Core/Src/hardware/potentiometer.c
+++ firmware/node-drive-controller/Core/Src/hardware/potentiometer.c
@@ -2,21 +2,76 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #include "main.h"
 
 extern ADC_HandleTypeDef hadc1;
 
-static uint32_t adc_value;
-static HAL_StatusTypeDef adc_status; // are adc readings currently possible?
+static uint32_t potentiometer_value;
 static uint32_t last_attempted_sample_tick; // last system time when reading was attempted
 static uint32_t last_valid_sample_tick; // last system time when reading was successful
+static bool is_available_last_attempted_sample_tick;
+static bool is_available_last_valid_sample_tick;
 
-static PotentiometerStatus_t potentiometer_status;
+static PotentiometerStatus_t potentiometer_status = POTENTIOMETER_STATUS_NOT_AVAILABLE;
 
+
+PotentiometerStatus_t get_potentiometer_value(uint32_t *value) {
+	if (value == NULL) {
+		return POTENTIOMETER_STATUS_INVALID_ARGUMENT;
+	}
+
+	if (potentiometer_status == POTENTIOMETER_STATUS_OK) {
+		*value = potentiometer_value;
+	}
+	return potentiometer_status;
+}
+
+// shall only be called if potentiometer value was tried to be read
+PotentiometerStatus_t get_last_attempted_sample_tick(uint32_t *last_attempted_sample_tick_out) {
+	if (last_attempted_sample_tick_out == NULL) {
+		return POTENTIOMETER_STATUS_INVALID_ARGUMENT;
+	}
+
+	if (!is_available_last_attempted_sample_tick) {
+		// potentiometer_task() was never called
+		return POTENTIOMETER_STATUS_NOT_AVAILABLE;
+	}
+	*last_attempted_sample_tick_out = last_attempted_sample_tick;
+	return POTENTIOMETER_STATUS_OK;
+}
+
+PotentiometerStatus_t get_last_valid_sample_tick(uint32_t *last_valid_sample_tick_out) {
+	if (last_valid_sample_tick_out == NULL) {
+		return POTENTIOMETER_STATUS_INVALID_ARGUMENT;
+	}
+
+	if (!is_available_last_valid_sample_tick) {
+		// potentiometer_task() was never called successfully
+		return POTENTIOMETER_STATUS_NOT_AVAILABLE;
+	}
+	*last_valid_sample_tick_out = last_valid_sample_tick;
+	return POTENTIOMETER_STATUS_OK;
+}
+
+
+PotentiometerStatus_t potentiometer_init(void) {
+	HAL_StatusTypeDef adc_status;
+	adc_status = HAL_ADCEx_Calibration_Start(&hadc1);
+	if (adc_status != HAL_OK) {
+		printf("Error during adc-calibration in potentiometer-initialization");
+		return POTENTIOMETER_STATUS_ERROR;
+	}
+
+	return POTENTIOMETER_STATUS_OK;
+}
 
 static PotentiometerStatus_t read_potentiometer(void) {
+	HAL_StatusTypeDef adc_status; // are adc readings currently possible?
+
 	last_attempted_sample_tick = HAL_GetTick();
+	is_available_last_attempted_sample_tick = true; // written every time read_potentiometer is called - TODO: check if this is professional
 
 	adc_status = HAL_ADC_Start(&hadc1);
 	if (adc_status != HAL_OK) { // simplified check whether adc_status is working
@@ -29,8 +84,8 @@ static PotentiometerStatus_t read_potentiometer(void) {
 		return POTENTIOMETER_STATUS_ERROR;
 	}
 
-	adc_value = HAL_ADC_GetValue(&hadc1);
-	printf("ADC-Wert ist: %lu\r\n", adc_value); // only for learning purposes
+	potentiometer_value = HAL_ADC_GetValue(&hadc1);
+	printf("ADC-Wert ist: %lu\r\n", potentiometer_value); // only for learning purposes
 
 
 	adc_status = HAL_ADC_Stop(&hadc1);
@@ -38,27 +93,11 @@ static PotentiometerStatus_t read_potentiometer(void) {
 		return POTENTIOMETER_STATUS_ERROR;
 	}
 	last_valid_sample_tick = HAL_GetTick();
+	is_available_last_valid_sample_tick = true; // currently written every time - TODO: check if this is professional
 
 	return POTENTIOMETER_STATUS_OK;
 }
 
-
-PotentiometerStatus_t potentiometer_init(void) {
-	adc_status = HAL_ADCEx_Calibration_Start(&hadc1);
-	if (adc_status != HAL_OK) {
-		printf("Error during adc-calibration in potentiometer-initialization");
-		return POTENTIOMETER_STATUS_ERROR;
-	}
-
-	return POTENTIOMETER_STATUS_OK;
-}
-
-PotentiometerStatus_t get_potentiometer_value(uint32_t *value) {
-	if (potentiometer_status == POTENTIOMETER_STATUS_OK) {
-		*value = adc_value;
-	}
-	return potentiometer_status;
-}
 
 PotentiometerStatus_t potentiometer_task(void) {
 	potentiometer_status = read_potentiometer();
@@ -66,6 +105,6 @@ PotentiometerStatus_t potentiometer_task(void) {
 	if (potentiometer_status != POTENTIOMETER_STATUS_OK) {
 		printf("Error during potentiometer task\r\n");
 	}
-	return POTENTIOMETER_STATUS_OK;
+	return potentiometer_status;
 }
 
